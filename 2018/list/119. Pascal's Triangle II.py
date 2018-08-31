a2ef5bc0c8ec88f65474c9f7e54d2836e244a05b
FanWu <1140989765@qq.com>
2018-08-31 15:06:46 +0800

    Create 119. Pascal's Triangle II.py

---
+++ 2018/list/119. Pascal's Triangle II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        row = [1]
+        for _ in range(rowIndex):
+            row = [x + y for x, y in zip([0]+row, row+[0])]#注意zip函数的用法
+        return row
