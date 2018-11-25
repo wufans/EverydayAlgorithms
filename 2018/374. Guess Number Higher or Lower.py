abb2a00f4f9396bdb6212022d323fc2d58f84d15
FanWu <1140989765@qq.com>
2018-11-25 18:13:09 +0800

    Create 374. Guess Number Higher or Lower.py

---
+++ 2018/374. Guess Number Higher or Lower.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#@author: WuFan
+"""
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+-1 : My number is lower
+ 1 : My number is higher
+ 0 : Congrats! You got it!
+Example :
+
+Input: n = 10, pick = 6
+Output: 6
+"""
+
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low = 1
+        high = n
+        while low <= high:
+            mid = (low + high) // 2
+            res = guess(mid)
+            if res == 0:
+                return mid
+            elif res == -1:
+                high = mid - 1
+            else:
+                low = mid + 1
