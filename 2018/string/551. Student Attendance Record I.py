4de9aa276fefd8587777f323e30f92aad99cbcbf
FanWu <1140989765@qq.com>
2018-12-05 20:13:22 +0800

    Create 551. Student Attendance Record I.py

---
+++ 2018/string/551. Student Attendance Record I.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#@author: WuFan
+
+"""
+You are given a string representing an attendance record for a student. The record only contains the following three characters:
+'A' : Absent.
+'L' : Late.
+'P' : Present.
+A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
+
+You need to return whether the student could be rewarded according to his attendance record.
+
+Example 1:
+Input: "PPALLP"
+Output: True
+Example 2:
+Input: "PPALLL"
+Output: False
+"""
+
+
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # return len(s.split('A'))<=2 and s.find('LLL')==-1
+        return not s.count('LLL') and s.count('A') <= 1
