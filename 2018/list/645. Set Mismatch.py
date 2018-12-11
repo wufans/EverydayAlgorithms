c74a91d73cf27eabc12756b3234d9b2b45d4391e
FanWu <1140989765@qq.com>
2018-12-11 17:00:29 +0800

    Create 645. Set Mismatch.py

---
+++ 2018/list/645. Set Mismatch.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#@author: WuFan
+"""
+The set S originally contains numbers from 1 to n. But unfortunately, due to the data error, one of the numbers in the set got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+Given an array nums representing the data status of this set after the error. Your task is to firstly find the number occurs twice and then find the number that is missing. Return them in the form of an array.
+
+Example 1:
+Input: nums = [1,2,2,4]
+Output: [2,3]
+Note:
+The given array size will in the range [2, 10000].
+The given array's numbers won't have any order.
+"""
+
+
+class Solution(object):
+    def findErrorNums(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        #my one line solultion 81.8(%
+        #return [sum(nums) - sum(set(nums)), ((1 + len(nums)) * len(nums)) / 2 - sum(set(nums))]
+
+        nsum = sum(nums)
+        ntarget = (1 + len(nums)) * len(nums) / 2
+        nset = set(nums)
+        nsetsum = sum(nset)
+        return [nsum - nsetsum, ntarget - nsetsum]
