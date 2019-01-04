70709d754665ae8f3ee32c62ad672dd881c7470c
FanWu <1140989765@qq.com>
2019-01-04 23:21:57 +0800

    Update Solution6.java

--- Java/Offer/Solution6.java
+++ Java/Offer/Solution6.java
@@ -7,6 +7,7 @@
 package offer;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Stack;
 
 public class Solution6 {
@@ -23,6 +24,44 @@ public class Solution6 {
         ArrayList<Integer> ret = new ArrayList<>();
         while (!stack.isEmpty())
             ret.add(stack.pop());
+        return ret;
+    }
+    //使用递归
+    public ArrayList<Integer> printListFromTailToHead2(ListNode listNode) {
+        ArrayList<Integer> ret = new ArrayList<>();
+        if (listNode != null) {
+            ret.addAll(printListFromTailToHead(listNode.next));
+            ret.add(listNode.val);
+        }
+        return ret;
+    }
+    //头插法
+    public ArrayList<Integer> printListFromTailToHead3(ListNode listNode) {
+        // 头插法构建逆序链表
+        ListNode head = new ListNode(-1);
+        while (listNode != null) {
+            ListNode memo = listNode.next;
+            listNode.next = head.next;
+            head.next = listNode;
+            listNode = memo;
+        }
+        // 构建 ArrayList
+        ArrayList<Integer> ret = new ArrayList<>();
+        head = head.next;
+        while (head != null) {
+            ret.add(head.val);
+            head = head.next;
+        }
+        return ret;
+    }
+    //Collections.reverse()
+    public ArrayList<Integer> printListFromTailToHead4(ListNode listNode) {
+        ArrayList<Integer> ret = new ArrayList<>();
+        while (listNode != null) {
+            ret.add(listNode.val);
+            listNode = listNode.next;
+        }
+        Collections.reverse(ret);
         return ret;
     }
 }
