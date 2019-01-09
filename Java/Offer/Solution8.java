862c3f2cddd5e249992e6c08397ec5d24538ff63
FanWu <1140989765@qq.com>
2019-01-09 20:45:24 +0800

    Create Solution8.java

---
+++ Java/Offer/Solution8.java
@@ -0,0 +1,33 @@
+/**
+ * @Author: WuFan
+ * @Date: 2019/1/9 20:37
+ */
+
+/*
+* 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。
+注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。*/
+
+//如果一个节点的右子树不为空，那么该节点的下一个节点是右子树的最左节点；
+//否则，向上找第一个左链接指向的树包含该节点的祖先节点。
+package offer;
+
+public class Solution8 {
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+        if(pNode.right!=null){
+            TreeLinkNode node = pNode.right;
+            while(node.left!=null){
+                node=node.left;
+            }
+            return node;
+        }else{
+            while(pNode.next!=null){
+                TreeLinkNode parent = pNode.next;
+                if(parent.left == pNode){
+                    return parent;
+                }
+                pNode = pNode.next;
+            }
+        }
+        return null;
+    }
+}
