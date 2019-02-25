ed2accdd045c9edbec2ef20e9d73ec39efecf393
FanWu <1140989765@qq.com>
2019-02-25 23:29:26 +0800

    Update Solution8.java

--- Java/Offer/Solution8.java
+++ Java/Offer/Solution8.java
@@ -30,4 +30,21 @@ public class Solution8 {
         }
         return null;
     }
+    private Map<Integer, Integer> indexForInOrders = new HashMap<>()
+; 
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        for (int i = 0; i < in.length; i++)
+        indexForInOrders.put(in[i], i);
+        return reConstructBinaryTree(pre, 0, pre.length - 1, 0);
+} 
+    private TreeNode reConstructBinaryTree(int[] pre, int preL, int preR, int inL) {
+        if (preL > preR)
+            return null;
+        TreeNode root = new TreeNode(pre[preL]);
+        int inIndex = indexForInOrders.get(root.val);
+        int leftTreeSize = inIndex - inL;
+        root.left = reConstructBinaryTree(pre, preL + 1, preL + leftTreeSize, inL);
+        root.right = reConstructBinaryTree(pre, preL + leftTreeSize  + 1, preR, inL + leftTreeSize + 1);
+        return root;
+}
 }
