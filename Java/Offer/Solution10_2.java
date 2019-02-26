be4b3d6b04ade5b84a1debfaf83ad683e2e86fdb
FanWu <1140989765@qq.com>
2019-02-26 18:13:51 +0800

    Update Solution10_2.java

--- Java/Offer/Solution10_2.java
+++ Java/Offer/Solution10_2.java
@@ -4,10 +4,17 @@
  */
 
 package offer;
+
+import java.util.Arrays;
+
 /*
-* 跳台阶
+* 1. 青蛙跳台阶
 * 一只青蛙一次可以跳上 1 级台阶，也可以跳上 2 级。求该青蛙跳上一个 n 级的台阶
-总共有多少种跳法。*/
+总共有多少种跳法。
+   2. 变态跳台阶
+   一只青蛙一次可以跳上 1 级台阶，也可以跳上 2 级... 它也可以跳上 n 级。求该青
+蛙跳上一个 n 级的台阶总共有多少种跳法。
+*/
 public class Solution10_2 {
     public int JumpFloor(int n){
         if(n <=2){
@@ -30,5 +37,26 @@ public class Solution10_2 {
 //            pre1 = result;
 //        }
 //        return result;
+    }
+
+    public int JumpFloorII(int target){
+//        if(target <=2){
+//            return target;
+//        }
+//        int number[] = new int[target];
+//        number[0] = 1;
+//        number[1] = 2;
+//        int number_sum = 3;
+//        for(int i = 2; i<= target-1;i++){
+//            number[i] = number_sum + 1;
+//            number_sum = number_sum*2 + 1;
+//        }
+//        return number[target-1];
+        int[] dp = new int[target];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < target; i++)
+            for (int j = 0; j < i; j++)
+                dp[i] += dp[j];
+        return dp[target - 1];
     }
 }
