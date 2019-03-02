4a76e71dc9b5fe9af33aa2866d0341fae3404c0e
FanWu <1140989765@qq.com>
2019-03-02 17:54:23 +0800

    Update Solution15.java

--- Java/Offer/Solution15.java
+++ Java/Offer/Solution15.java
@@ -8,6 +8,7 @@ package offer;
 * 二进制中 1 的个数
 * 输入一个整数，输出该数二进制表示中 1 的个数。*/
 public class Solution15 {
+    //这个方法不对，当n=-2147483648不能通过
     public int NumberOf1(int n){
         int res = 0;
         while( n>0){
