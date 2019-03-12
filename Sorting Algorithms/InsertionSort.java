9461ad0f041982b45be2a92c45fe828a0780e792
FanWu <1140989765@qq.com>
2019-03-12 19:39:22 +0800

    Create InsertionSort.java

---
+++ Sorting Algorithms/InsertionSort.java
@@ -0,0 +1,36 @@
+/**
+ * @Author: WuFan
+ * @Date: 2019/3/12 19:18
+ */
+
+package sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] testList = new int[]{-6,-3,-2,7,-15,1,2,2};
+        int[] testList2 = new int[]{};
+        System.out.println(Arrays.toString(insertSort(testList)));
+    }
+    public static int[] insertSort(int[] list){
+        if(list.length<=1){
+            return list;
+        }
+        for (int i = 1; i < list.length ; i++) {
+            int temp = list[i];
+            for (int j = i-1; j >= 0; j--) {
+                if(list[j]>temp){
+                    list[j+1] = list[j];
+                    if(j == 0)
+                        list[0] = temp;
+                }else{
+                    list[j+1] = temp;
+                    break;
+                }
+
+            }
+        }
+        return list;
+    }
+}
