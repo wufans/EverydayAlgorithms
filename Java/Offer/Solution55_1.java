16a33fbdac5464e8436e43bf0fb5d5edb68407d7
FanWu <1140989765@qq.com>
2019-03-17 20:12:19 +0800

    Update and rename Solution55.java to Solution55_1.java

--- Java/Offer/Solution55_1.java
+++ Java/Offer/Solution55_1.java
@@ -7,7 +7,7 @@ package offer;
 /*
 * 二叉树的深度
 * 输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。*/
-public class Solution55 {
+public class Solution55_1 {
     private int res = 0;
     public int TreeDepth(TreeNode root) {
         findDepth(root,0);
