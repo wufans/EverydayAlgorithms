7162a32eca0b43f84374015d02d5866f1e84b49a
FanWu <1140989765@qq.com>
2019-04-16 21:09:42 +0800

    Create Solution23.java

---
+++ Java/leetcode/Solution23.java
@@ -0,0 +1,57 @@
+/**
+ * @Author: WuFan
+ * @Date: 2019/4/16 20:58
+ */
+
+package leetcode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/*
+* Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+*/
+public class Solution23 {
+//用小根堆实现
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists==null||lists.length==0) return null;
+
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.length,new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                if (o1.val<o2.val)
+                    return -1;
+                else if (o1.val==o2.val)
+                    return 0;
+                else
+                    return 1;
+            }
+        });
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail=dummy;
+
+        for (ListNode node:lists)
+            if (node!=null)
+                queue.add(node);
+
+        while (!queue.isEmpty()){
+            tail.next=queue.poll();
+            tail=tail.next;
+
+            if (tail.next!=null)
+                queue.add(tail.next);
+        }
+        return dummy.next;
+    }
+}
